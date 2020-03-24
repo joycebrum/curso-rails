9da498991679429df6b16aeb497ea7cdd9d3bb91
Joyce <joycebrumu.u@gmail.com>
2020-03-24 15:46:09 -0300

    aula 64 -creating your own tasks

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cadastrando moedas"
+
+Coin.create!(
+    description: 'Bitcoin',
+    acronym: 'BTC',
+    url_image: 'https://en.bitcoin.it/w/images/en/5/50/Bitcoin.png'
+)
+
+Coin.create!(
+    description: 'Ethereum',
+    acronym: 'ETH',
+    url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png'
+)
+
+puts "Moedas cadastradas com sucesso"
